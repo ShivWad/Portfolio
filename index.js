fb88c17e77e677f953fb5de73a733cc9e1bf5aec
Shivam Wadhawan <63960743+ShivWad@users.noreply.github.com>
2022-09-28 08:58:06 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+let greeting = ["Hello", "Welcome"];
+
+let greetingName = "I am Shivam";
+
+let speed = 110;
+let i = 0;
+
+function writeGreeting() {
+  if (i < greeting[0].length) {
+    document.getElementById("greeting").innerHTML += greeting[0].charAt(i);
+    i++;
+    setTimeout(writeGreeting, speed);
+  }
+}
+
+
+let j = 0;
+function writeIntroduction() {
+  if (j < greeting[1].length) {
+    document.getElementById("greet-2").innerHTML += greeting[1].charAt(j);
+    j++;
+    setTimeout(writeIntroduction, speed);
+  }
+}
+
+let k=0;
+
+function writeName() {
+    if (k < greetingName.length) {
+      document.getElementById("intro").innerHTML += greetingName.charAt(k);
+      k++;
+      setTimeout(writeName, speed);
+    }
+  }
+
+
+function yo(){
+
+    console.log('yoyoy');
+}
+
+writeGreeting();
+
+setTimeout(writeIntroduction, 850);
+
+setTimeout(writeName, 2000)
