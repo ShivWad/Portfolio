7aef0009af74bd756ecd4bf266492affa2386672
ShivWad <shivamwadhawan2000@gmail.com>
2022-10-05 17:04:31 +0530

    skills

--- index.js
+++ index.js
@@ -32,10 +32,20 @@ function writeName() {
   }
 }
 
-
-
 writeGreeting();
 
 setTimeout(writeIntroduction, 850);
 
 setTimeout(writeName, 2000);
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    entry.target.classList.toggle('show', entry.isIntersecting);
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el)=>observer.observe(el));
+
+
+console.log('oaihjsdlkasjdl')
